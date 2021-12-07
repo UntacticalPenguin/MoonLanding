0d7a8ce8474774b10e85fff8fa2089d37bc003a9
UntacticalPenguin <ilja.afonin@gmail.com>
2021-12-07 23:16:06 +0100

    testing if compiling is working

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hello = void 0;
 let world = 'world';
+document.body.style.backgroundColor = "green";
 function hello(kek = world) {
     return `Hello ${kek}! `;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 let world = 'world';
+document.body.style.backgroundColor = "green";
 
 export function hello(kek = world): string {
   return `Hello ${kek}! `;
