2ef3e358553ec122533a6ff947cda21dcf97ae9d
UntacticalPenguin <ilja.afonin@gmail.com>
2021-12-09 00:07:41 +0100

    Added provisional animated star background, started working on animation logic with canvas

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,28 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
 let world = 'world';
-let bob = document.querySelector("body");
-bob.style.backgroundColor = "green";
-function hello(kek = world) {
-    return `Hello ${kek}! `;
+let canvas, context;
+function init() {
+    let canvas = document.getElementById('gameCanvas');
+    let context = canvas.getContext('2d');
+    context.beginPath();
+    context.strokeRect(50, 35, 50, 50);
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.bezierCurveTo(30, 40, 50, 150, 200, 80);
+    context.stroke();
+    context.closePath();
 }
-exports.hello = hello;
+class Shuttle {
+    constructor(x = 0, y = 0, width = 0, height = 0, fillColor = '', strokeColor = '', strokeWidth = 2) {
+        this.x = Number(x);
+        this.y = Number(y);
+        this.width = Number(width);
+        this.height = Number(height);
+        this.fillColor = fillColor;
+        this.strokeColor = strokeColor;
+        this.strokeWidth = strokeWidth;
+    }
+    draw() {
+    }
+}
+document.addEventListener('DOMContentLoaded', init);
 //# sourceMappingURL=index.js.map
