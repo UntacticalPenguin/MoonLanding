ed87bbb995b0ff050c26760834dfcd90e4c0a63d
UntacticalPenguin <ilja.afonin@gmail.com>
2021-12-11 19:45:16 +0100

    added start button and star animation play

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,22 @@ document.addEventListener('DOMContentLoaded', ()=>{
     //console.log(temp_el.style.animationPlayState)
     //console.log(compstyles.getPropertyValue("animation-play-state"))
   }
-  stars[0].style.animationPlayState = "running"
+  let start_div = document.createElement('div');
+  document.body.insertBefore(start_div, document.body.firstChild)
+  start_div.classList.add("start-div")
+  let start_button = document.createElement('div');
+  start_div.appendChild(start_button)
+  start_button.classList.add("start-button");
+  start_button.textContent = "START"
+  start_button.addEventListener("mousedown", () =>{
+    for (let i=0; i<3; i++){
+      stars[i].style.animationPlayState = "running"
+    }
+    start_div.classList.add("hidden-class");
 })
+
+})
+
+
+
+
